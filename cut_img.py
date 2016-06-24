d3f4897bdcfe1a27861ea745a375a27e134132cd
zxc0258741 <zxc0258741@gmail.com>
2016-06-24 21:59:58 +0800

    first upload

---
+++ cut_img.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import Image
+
+im=Image.open('/home/lcw/下載/Institute_for_Information_Industry.svg_-624x545.png')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/tensorflow/Institute_for_Information_Industry.svg_-624x545.jpg')
+
+im=Image.open('/home/lcw/下載/20160504cafe_trump.jpg')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/20160504cafe_trump.jpg')
+
+im=Image.open('/home/lcw/下載/c4e7e0a7-b5b1-4038-9ddd-10a47315f5cb.jpg')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/c4e7e0a7-b5b1-4038-9ddd-10a47315f5cb.jpg')
+
+
+im=Image.open('/home/lcw/下載/phpyqkVW6.jpg')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/phpyqkVW6.jpg')
+
+
+im=Image.open('/home/lcw/下載/570cbfd0edc2cad8d4c8273d.jpg')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/570cbfd0edc2cad8d4c8273d.jpg')
+
+
+im=Image.open('/home/lcw/下載/570cbfffedc2cad8d4c8b9d6.jpg')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/570cbfffedc2cad8d4c8b9d6.jpg')
+
+
+im=Image.open('/home/lcw/下載/570cd062edc2cad8d4f89e04.jpg')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/t570cd062edc2cad8d4f89e04.jpg')
+
+
+im=Image.open('/home/lcw/下載/570cd5daedc2cad8d4081065.jpg')
+size =(300,300)
+nim = im.resize(size)
+nim.save('/home/lcw/下載/570cd5daedc2cad8d4081065.jpg')
+
+
+
+
+
